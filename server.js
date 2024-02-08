de2bcae77236d10cb27db7307c436d66f34244fd
TanishkaLimkar <tanishkalimkar56@gmail.com>
2024-02-08 22:48:50 +0530

    frontend link updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const errorMiddleware = require("./middlewares/error-middleware");
 
 // handle CORS
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://frontend-two-gilt-78.vercel.app/",
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
 };
